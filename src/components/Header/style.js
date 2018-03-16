2cadfc96f51a102dcbe6bb16a7f1fd957f1a1676
Christian Rebelsky <christian.rebelsky@gmail.com>
2018-03-16 22:38:41 +0100

    add grid-styled, add sample styles

---
+++ src/components/Header/style.js
@@ -0,0 +1,15 @@
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: rebeccapurple;
+  marginbottom: 1.45rem;
+`;
+
+export const Nav = styled.div`
+`;
+
+export const NavLink = styled(Link)`
+   color: white;
+   text-decoration: none;
+`;
